398ecf8b4b6b5709375b50520588edbb47afc73a
Tony <ling.java@gmail.com>
2013-08-05 21:54:23 +0800

    update init.el

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -1,13 +1,13 @@
 ;initial and setting auto-complete
-(unless (require 'auto-complete nil 'noerror)
-  (el-get-install "auto-complete"))
+(require 'auto-complete-config)
 
 (setq auto-complete-dict-dir
       (expand-file-name "auto-complete/dict"
 			el-get-root-dir))
-(require 'auto-complete-config)
+
 (add-to-list 'ac-dictionary-directories
 	     auto-complete-dict-dir)
+
 (setq ac-auto-show-menu 0.5)
 (ac-config-default)
 
--- init.el
+++ init.el
@@ -1,15 +1,28 @@
+;;add $HOME/.emacs.d to load-path
 (add-to-list 'load-path user-emacs-directory)
 
+;;define the el-get root directory
 (setq el-get-root-dir
       (expand-file-name "el-get"
 		        user-emacs-directory))
 
+;;define the default recipes directory
 (setq el-get-recipes-path
-      (expand-file-name "recipes"
+       (expand-file-name "el-get/recipes"
 			el-get-root-dir))
 
-(add-to-list 'load-path el-get-root-dir)
+;;define core path of el-get
+(setq el-get-core-path
+      (expand-file-name "el-get"
+			el-get-root-dir))
 
+;;add el-get core path to load-path
+(add-to-list 'load-path el-get-core-path)
+
+;;settiing the $HOME/.emacs.d as the el-get user package
+(setq el-get-user-package-directory user-emacs-directory)
+
+;;initial el-get package
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
@@ -17,8 +30,15 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
+;;add default el-get recipes path
 (add-to-list 'el-get-recipe-path el-get-recipes-path)
-(el-get 'sync)
+
+(setq my-packages
+      (append
+       '(auto-complete)))
+
+;;sync packages
+(el-get 'sync my-packages)
 
 (require 'init-appearance)
-(require 'init-auto-complete)
+;(require 'init-auto-complete)
