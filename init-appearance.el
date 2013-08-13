a7e2e713dbbddf6519b5823e316177b143301fef
jsuper <ling.java@gmail.com>
2013-08-13 15:26:57 +0800

    update the encoding

--- init-appearance.el
+++ init-appearance.el
@@ -65,11 +65,11 @@
 
 ;;; ### Coding ###
 ;;; ---编码设置
-(setq default-buffer-file-coding-system 'utf-8-unix)            ;缓存文件编码
-(setq default-file-name-coding-system 'utf-8-unix)              ;文件名编码
-(setq default-keyboard-coding-system 'utf-8-unix)               ;键盘输入编码
-(setq default-process-coding-system '(utf-8-unix . utf-8-unix)) ;进程输入输出编码
-(setq default-sendmail-coding-system 'utf-8-unix)               ;发送邮件编码
-(setq default-terminal-coding-system 'utf-8-unix)               ;终端编码
+;;(setq default-buffer-file-coding-system 'utf-8-unix)            ;缓存文件编码
+;;(setq default-file-name-coding-system 'utf-8-unix)              ;文件名编码
+;;(setq default-keyboard-coding-system 'utf-8-unix)               ;键盘输入编码
+;;(setq default-process-coding-system '(utf-8-unix . utf-8-unix)) ;进程输入输出编码
+;;(setq default-sendmail-coding-system 'utf-8-unix)               ;发送邮件编码
+;;(setq default-terminal-coding-system 'utf-8-unix)               ;终端编码
 
 (provide 'init-appearance)
