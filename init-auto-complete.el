235ca35ef03b661e63543681cde06e69df5e4b04
jsuper <ling.java@gmail.com>
2013-08-13 15:27:33 +0800

    add ac-sources, disable ruby-ac-complete and so on

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -8,7 +8,19 @@
 (add-to-list 'ac-dictionary-directories
 	     auto-complete-dict-dir)
 
+(set-default 'ac-sources
+             '(ac-source-abbrev
+               ac-source-dictionary
+               ac-source-yasnippet
+               ac-source-words-in-buffer
+               ac-source-words-in-same-mode-buffers
+               ac-source-semantic))
+
+(add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+;(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+;(add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+;(add-hook 'css-mode-hook 'ac-css-mode-setup)
 (setq ac-auto-show-menu 0.5)
-(ac-config-default)
+(global-auto-complete-mode t)
 
 (provide 'init-auto-complete)
