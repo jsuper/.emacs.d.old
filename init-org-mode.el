2bec646d7a199f9a99991881c12b6c41a111b8ac
Tony <ling.java@gmail.com>
2013-08-28 00:25:30 +0800

    add org-mode configuration

---
+++ init-org-mode.el
@@ -0,0 +1,12 @@
+
+;configuration for org-mode
+
+(global-font-lock-mode t)
+
+;; org-mode colors
+(setq org-todo-keyword-faces
+      '(
+        ("INPR" . (:foreground "yellow" :weight bold))
+        ("DONE" . (:foreground "green" :weight bold))
+        ("IMPEDED" . (:foreground "red" :weight bold))
+        ))
