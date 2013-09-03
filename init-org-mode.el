7d217179d2b8c9820d625861b3fd4c38af9924d1
jsuper <ling.java@gmail.com>
2013-09-03 17:34:13 +0800

    add org-mode jekyll related settings

--- init-org-mode.el
+++ init-org-mode.el
@@ -1,6 +1,6 @@
 
 ;configuration for org-mode
-
+(require 'org-publish-setting)
 (global-font-lock-mode t)
 
 ;; org-mode colors
@@ -10,3 +10,5 @@
         ("DONE" . (:foreground "green" :weight bold))
         ("IMPEDED" . (:foreground "red" :weight bold))
         ))
+
+(provide 'init-org-mode)
