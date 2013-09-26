acb9abdadfbfff02554100ea6a458c76d2b8b81a
jsuper <ling.java@gmail.com>
2013-09-26 17:44:09 +0800

    update

--- init-appearance.el
+++ init-appearance.el
@@ -50,12 +50,20 @@
 (setq show-paren-style 'parenthess)
 (setq make-backup-files nil)
 
+;;----------------------------------------------------
+;; Set default coding environment
+;;----------------------------------------------------
+(setq-default
+ current-language-environment "UTF-8"
+ default-input-method "chinese-py"
+ locale-coding-system 'utf-8
+ default-terminal-coding-system 'utf-8
+ default-file-name-coding-system 'utf-8
+ default-keyboard-coding-system 'utf-8
+ default-buffer-file-coding-system 'utf-8
+ default-sendmail-coding-system 'utf-8)
+
 ;;; setting locale and coding
-(setq current-language-environment "UTF-8")
-(setq default-input-method "chinese-py")
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (setq echo-keystrokes 0.1)
@@ -65,15 +73,6 @@
 (blink-cursor-mode -1)
 (setq ring-bell-function 'ignore)
 (setq print-escape-newlines t)
-
-;;; ### Coding ###
-;;; ---编码设置
-(setq default-buffer-file-coding-system 'utf-8-unix)            ;缓存文件编码
-(setq default-file-name-coding-system 'utf-8-unix)              ;文件名编码
-(setq default-keyboard-coding-system 'utf-8-unix)               ;键盘输入编码
-(setq default-process-coding-system '(utf-8-unix . utf-8-unix)) ;进程输入输出编码
-(setq default-sendmail-coding-system 'utf-8-unix)               ;发送邮件编码
-(setq default-terminal-coding-system 'utf-8-unix)               ;终端编码
 
 (global-set-key (kbd "C-SPC") nil)
 
