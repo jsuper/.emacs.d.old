b0b80a403bab7ea81fa72c0d76c00eab778fb855
jsuper <ling.java@gmail.com>
2014-01-08 15:08:52 +0800

    Modify the face of org-mod headline 1,2

--- init-custom.el
+++ init-custom.el
@@ -10,4 +10,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:background "grey22" :foreground "#FFFFFF" :box (:line-width 1 :color "#073642" :style unspecified)))))
- '(mode-line-buffer-id ((t (:foreground "#FFFFFF" :weight bold)))))
+ '(mode-line-buffer-id ((t (:foreground "#FFFFFF" :weight bold))))
+ '(org-level-1 ((t (:inherit outline-1 :foreground "dodger blue" :weight bold :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :foreground "#edd400" :weight bold :height 1.2)))))
